a46a3641cdc027a779eae01576ee80df89605df1
Adam Lay <adam93.lay@gmail.com>
2026-07-09 09:41:03 +0100

    .

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -12,7 +12,7 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 const isLocal = (conn: string) => conn?.includes("localhost") || conn?.includes("hyperion");
 const mainAdapter = new PrismaPg({
   connectionString: env.DATABASE_URL,
-  ssl: isLocal(env.DATABASE_URL) ? false : { rejectUnauthorized: false },
+  ssl: false, //isLocal(env.DATABASE_URL) ? false : { rejectUnauthorized: false },
 });
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter: mainAdapter });
